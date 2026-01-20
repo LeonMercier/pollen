dd5b8f61c7a1433f376b56ad009782634d3c196e
Leon Mercier <leonmercier24@gmail.com>
2026-01-20 15:47:30 +0200

    cleanup

--- README.md
+++ README.md
@@ -1,8 +1,17 @@
 ## Extract
 
+## Dev environment
+
+npm install -g azure-functions-core-tools@4
+npm install -g azurite
+mkdir azure_functions && cd azure_functions
+func init . --python
+
 ### Environment variables needed
 
 - CDSAPI_URL
 - CDSAPI_KEY
 
-cdsapi expects api keys in $HOME/.cdsapirc if they dont exist otherwise
+for local development these can be in azure_functions/local.settings.json in "Values"
+
+cdsapi tries to read api keys from $HOME/.cdsapirc if they dont exist otherwise
--- src/extract/extract.py
+++ src/extract/extract.py
@@ -3,32 +3,33 @@ import cdsapi
 from azure.storage.blob import BlobServiceClient
 
 # TODO: return something?
-# TODO: import azure.storage.blob, take path to that as arg
 def download(api_url, api_key, storage_conn_str):
 
-    # This part from query builder ################
+    # This part from CAMS query builder ################
     dataset = "cams-europe-air-quality-forecasts"
     request = {
-        "variable": ["birch_pollen"],
-        "model": ["ensemble"],
-        "level": ["0"],
-        "date": ["2025-03-19/2025-03-19"],
-        "type": ["forecast"],
-        "time": ["00:00"],
-        "leadtime_hour": ["0"],
-        "data_format": "grib"
+    "variable": ["birch_pollen"],
+    "model": ["ensemble"],
+    "level": ["0"],
+    "date": ["2025-03-20/2025-03-20"],
+    "type": ["forecast"],
+    "time": ["00:00"],
+    "leadtime_hour": ["0"],
+    "data_format": "grib",
+    "area": [61, 23, 60, 24]
     }
-    #################
+    ####################################################
 
     client = cdsapi.Client()
 
     filename = 'result.grib'
 
-    # when filename is passed as argument, returns a string, not a Result 
+    # when filename is passed as argument, returns a string, NOT a Result 
     # object with a .download() method
     client.retrieve(dataset, request, filename)
 
-    # Now configure Blob storage and upload 
+
+    # Now configure Blob storage and upload ############
 
     blob_service = BlobServiceClient.from_connection_string(storage_conn_str)
 
@@ -45,7 +46,8 @@ def download(api_url, api_key, storage_conn_str):
         pass
 
     with open(file=filename, mode="rb") as data:
-        blob_client = container_client.upload_blob(name="sample-blob.txt", data=data, overwrite=True)
+        blob_client = container_client.upload_blob(
+            name="result.grib", data=data, overwrite=True)
 
     if os.path.exists(filename):
         os.remove(filename)
