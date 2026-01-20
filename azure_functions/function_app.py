a9792c749a47a6052e0e7fbc139a54a0df57d977
Leon Mercier <leonmercier24@gmail.com>
2026-01-20 15:15:19 +0200

    extractor MVP works now locally with azurite

--- azure_functions/function_app.py
+++ azure_functions/function_app.py
@@ -3,6 +3,7 @@ import datetime
 import json
 import logging
 import os
+import sys
 from pathlib import Path
 
 # add the repo root to our path
@@ -16,13 +17,17 @@ from src.extract.extract import download
 app = func.FunctionApp()
 
 # Crontab expression: at minute 5 of every hour of every day...
-@app.timer_trigger(schedule="0 5 * * * * *", arg_name="myTimer", run_on_startup=False,
+@app.timer_trigger(schedule="0 5 * * * *", arg_name="myTimer", run_on_startup=False,
               use_monitor=False) 
 def extract(myTimer: func.TimerRequest) -> None:
     
     if myTimer.past_due:
         logging.info('The timer is past due!')
 
-    download(api_url=os.environ['CDSAPI_URL'], api_key=os.environ['CDSAPI_KEY'])
+    download(
+        api_url=os.environ['CDSAPI_URL'],
+        api_key=os.environ['CDSAPI_KEY'],
+        storage_conn_str=os.environ.get("AzureWebJobsStorage")
+    )
 
     logging.info('Python timer trigger function executed.')
--- src/extract/extract.py
+++ src/extract/extract.py
@@ -1,9 +1,10 @@
+import os
 import cdsapi
 from azure.storage.blob import BlobServiceClient
 
 # TODO: return something?
 # TODO: import azure.storage.blob, take path to that as arg
-def download(api_url, api_key, storage_conn_str)
+def download(api_url, api_key, storage_conn_str):
 
     # This part from query builder ################
     dataset = "cams-europe-air-quality-forecasts"
@@ -20,11 +21,33 @@ def download(api_url, api_key, storage_conn_str)
     #################
 
     client = cdsapi.Client()
+
     filename = 'result.grib'
-    client.retrieve(dataset, request, filename).download()
+
+    # when filename is passed as argument, returns a string, not a Result 
+    # object with a .download() method
+    client.retrieve(dataset, request, filename)
+
+    # Now configure Blob storage and upload 
 
     blob_service = BlobServiceClient.from_connection_string(storage_conn_str)
+
+    # doesnt actually create anything on the remote, just creates the object 
+    # that represents the resource
     container_client = blob_service.get_container_client('bronze')
+
+    # Create container if it doesn't exist
+    from azure.core.exceptions import ResourceExistsError
+    try:
+        container_client.create_container()
+    except ResourceExistsError:
+        # Container already exists, that's fine
+        pass
 
     with open(file=filename, mode="rb") as data:
         blob_client = container_client.upload_blob(name="sample-blob.txt", data=data, overwrite=True)
+
+    if os.path.exists(filename):
+        os.remove(filename)
+    else:
+        print("No file to remove")
