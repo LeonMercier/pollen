95d227b7c3e2583db1608d1fe4b86321df0f0017
Leon Mercier <leonmercier24@gmail.com>
2026-02-17 14:25:52 +0200

    basic plot with plotly

--- notebooks/plot.py
+++ notebooks/plot.py
@@ -1,5 +1,6 @@
 # pyspark available by default in Databricks
 from pyspark.sql import SparkSession
+import plotly.express as px
 
 # NOTE: we could have skipped the whole SQL step and use just dataframes and
 # possibly parquet. But there will be other users of the data in the future
@@ -26,13 +27,26 @@ query = """
 
 try:
     df = spark.read.jdbc(
-        jdbc_url, "dbo.pollen.forecast", properties=connection_properties
+        jdbc_url, "dbo.pollen_forecast", properties=connection_properties
     )
 except Exception as e:
     print(f"WARNING: Could not read from database: {str(e)}")
     raise
 
-subset = df.filter((df.latitude == 60.95) & (df.longitude == 23.05))
+df = df.filter((df.latitude == 60.95) & (df.longitude == 23.05))
+df = df.select(df.forecast_time, df.constituent_value, df.constituent_type)
+# pyspark specific way to visualize interactively
+df.display()
 
-# pyspark specific way visualize interactively
-subset.display()
+# plotly will connect points in the order that they are in the data
+df = df.orderBy("forecast_time")
+
+fig = px.line(
+    df,
+    x="forecast_time",
+    y="constituent_value",
+    color="constituent_type",
+    title="Pollen grains in m3 of air",
+    markers=True,
+)
+fig.show()
