26baff21595c2eea11119d09f512a01252849e82
Leon Mercier <leonmercier24@gmail.com>
2026-02-19 14:41:01 +0200

    download a larger area, extract plotting logic into a function

--- notebooks/plot.py
+++ notebooks/plot.py
@@ -8,6 +8,52 @@ from azure.storage.blob import BlobServiceClient, ContentSettings
 # possibly parquet. But there will be other users of the data in the future
 # and so using SQL makes sense
 
+
+def plot(latitude, longitude, name, sqlconn):
+    # using f-strings to create SQL queries opens the door to SQL injection
+    # here we enforce the types to prevent that
+    lat = float(latitude)
+    lon = float(longitude)
+
+    # the 'AS' alias is needed because spark needs a subquery that it can then
+    # further wrap with another query
+    query = f"""
+        (SELECT start_date, forecast_time, constituent_type, constituent_value
+        FROM dbo.pollen_forecast
+        WHERE latitude = {lat} AND longitude = {lon}) AS filtered_data
+        """
+    try:
+        df = spark.read.jdbc(jdbc_url, query, properties=sqlconn)
+    except Exception as e:
+        print(f"WARNING: Could not read from database: {str(e)}")
+        raise
+
+    # expr means you can do SQL
+    # this is to get a nice x-axis with actual datetimes instead of just hours since
+    # start of forecast
+    df = df.withColumn(
+        "forecast_datetime", expr("DATEADD(hour, forecast_time, start_date)")
+    )
+
+    # pyspark specific way to visualize interactively
+    df.display()
+
+    # need to sort because plotly will connect points in the order that they are in the data
+    df = df.orderBy("forecast_datetime")
+
+    fig = px.line(
+        df,
+        x="forecast_datetime",
+        y="constituent_value",
+        color="constituent_type",
+        title="Pollen grains in m3 of air",
+        markers=True,
+    )
+    fig.show()
+
+    fig.write_html(f"/tmp/{name}.html", include_plotlyjs="cdn")
+
+
 # get secrets for SQL access
 sql_server = dbutils.secrets.get(scope="secrets", key="sql-server-fqdn")
 sql_username = dbutils.secrets.get(scope="secrets", key="sql-admin-username")
@@ -22,47 +68,17 @@ connection_properties = {
     "driver": "com.microsoft.sqlserver.jdbc.SQLServerDriver",
 }
 
-try:
-    df = spark.read.jdbc(
-        jdbc_url, "dbo.pollen_forecast", properties=connection_properties
-    )
-except Exception as e:
-    print(f"WARNING: Could not read from database: {str(e)}")
-    raise
+# locations we want to plot
+# available grid points are like x.05, x.15, x.25 ... x.95
+locations = [
+    {"lat": 60.15, "lon": 24.95, "name": "Helsinki"},
+    {"lat": 48.85, "lon": 2.35, "name": "Paris"},
+    {"lat": 41.85, "lon": 12.55, "name": "Rome"},
+]
 
-# trying to get Helsinki here
-# grid is 0.10 degrees increments
-df = df.filter((df.latitude == 60.15) & (df.longitude == 24.95))
-df = df.select(
-    df.start_date, df.forecast_time, df.constituent_value, df.constituent_type
-)
-
-# expr means you can do SQL
-# this is to get a nice x-axis with actual datetimes instead of just hours since
-# start of forecast
-df = df.withColumn(
-    "forecast_datetime", expr("DATEADD(hour, forecast_time, start_date)")
-)
-
-# pyspark specific way to visualize interactively
-df.display()
-
-# plotly will connect points in the order that they are in the data
-df = df.orderBy("forecast_datetime")
-
-fig = px.line(
-    df,
-    x="forecast_datetime",
-    y="constituent_value",
-    color="constituent_type",
-    title="Pollen grains in m3 of air",
-    markers=True,
-)
-fig.show()
-
-fig.write_html("/tmp/fig1.html", include_plotlyjs="cdn")
-
-# Upload to website
+# create plots and save to /tmp
+for l in locations:
+    plot(l["lat"], l["lon"], l["name"], connection_properties)
 
 # Get storage credentials from Databricks secrets
 storage_account_name = dbutils.secrets.get(
@@ -80,24 +96,26 @@ blob_service_client = BlobServiceClient(
 
 # Upload to $web container (static website hosting container)
 container_name = "$web"
-blob_name = "index.html"
 
-try:
-    blob_client = blob_service_client.get_blob_client(
-        container=container_name, blob=blob_name
-    )
-
-    with open("/tmp/fig1.html", "rb") as data:
-        blob_client.upload_blob(
-            data,
-            overwrite=True,
-            content_settings=ContentSettings(content_type="text/html"),
+for l in locations:
+    tmp_name = f"/tmp/{l["name"]}.html"
+    blob_name = f"{l["name"]}.html"
+    try:
+        blob_client = blob_service_client.get_blob_client(
+            container=container_name, blob=blob_name
         )
 
-except Exception as e:
-    print(f"ERROR uploading chart to blob storage: {str(e)}")
-    raise
+        with open(tmp_name, "rb") as data:
+            blob_client.upload_blob(
+                data,
+                overwrite=True,
+                content_settings=ContentSettings(content_type="text/html"),
+            )
 
-# Clean up local temporary file
-if os.path.exists("/tmp/fig1.html"):
-    os.remove("/tmp/fig1.html")
+    except Exception as e:
+        print(f"ERROR uploading chart to blob storage: {str(e)}")
+        raise
+
+    # Clean up local temporary file
+    if os.path.exists(tmp_name):
+        os.remove(tmp_name)
